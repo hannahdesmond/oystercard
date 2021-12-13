1ca4da68eb093239907e4f31eda80b726e2273ca
Hannah Desmond <desmond.hannah@googlemail.com>
2021-12-13 16:34:29 +0000

    Add #top_up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,9 @@ describe Oystercard do
   it 'Start with a balance of zero' do
     expect(subject.balance).to eq(0) 
   end
+  describe '#top_up' do
+    it 'increments the balance' do
+      expect { subject.top_up(10) }.to change { subject.balance }.by(10)
+    end
+  end
 end
