0c860ee1eb5101a9642dc05a47f6acf9c79d5773
Hannah Desmond <desmond.hannah@googlemail.com>
2021-12-30 15:05:27 +0000

    Add #finish method to JourneyLog class

--- lib/journeylog.rb
+++ lib/journeylog.rb
@@ -8,6 +8,13 @@ class JourneyLog
 
   def start(entry_station)
      @current_journey = @journey_class.new(entry_station)
+     @journeys << current_journey
+  end
+
+  def finish(exit_station)
+    current_journey
+    @journeys << @current_journey.finish(exit_station)
+    @current_journey = nil
   end
 
   private 
@@ -16,12 +23,3 @@ class JourneyLog
     @current_journey ||= @journey_class.new
   end
 end
-
-
-# the code is initialized by passing in the Journey
-# class. It is not instantiated until the journey
-# starts. #start instantiates the journey class
-# and passes in an entry station.
-
-# Journey class can be instantiated without 
-# passing in an entry station. It can be nil
--- spec/journeylog_spec.rb
+++ spec/journeylog_spec.rb
@@ -2,7 +2,7 @@ require 'journeylog'
 describe JourneyLog do
   let(:station) { double :station }
   let(:other_station) { double :other_station }
-  let(:journey) { double :journey, entry_station: station, exit_station: other_station }
+  let(:journey) { spy :journey, entry_station: station, exit_station: other_station}
   let(:journey_class) { double :journey_class, new: journey } 
   subject {described_class.new(journey_class: journey_class)}
   
@@ -10,10 +10,26 @@ describe JourneyLog do
     it 'holds a default list of past journeys' do
       expect(subject.journeys).to eq([])
     end
-    describe '#start'
+    describe '#start' do
       it 'starts a new journey with an entry station' do
         subject.start(station)
         expect(journey_class).to have_received(:new).with(station)
       end
+      it 'records a journey' do
+        subject.start(station)
+        expect(subject.journeys).to include(journey)
+      end
+    end
+    describe '#finish' do
+      it 'should add an exit station where there is an entry station' do
+        subject.start(station)
+        subject.finish(other_station)
+        expect(journey).to have_received(:finish)
+        subject.start(station)
+        subject.finish(other_station)
+        expect(subject.journeys).to include(journey, journey)
+      end
+    end
   end
+  
 end
